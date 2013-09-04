eaf3f3a2d23b0f3dc8727cf7e593c5ee530562dc
Michael La Plante <michael@michaellaplante.com>
2013-09-04 16:40:45 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,508 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>Barcamp Tampa Bay</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+  	<link href='http://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+  	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,400italic,600italic,700italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/flexslider.css">
+	<link rel="stylesheet" href="stylesheets/prettyPhoto.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+	<!--<link rel="stylesheet" href="stylesheets/bootstrap.css">-->
+    <!-- I ADDED THESE WEBFONTS -->
+    <link rel="stylesheet" href="stylesheets/webfonts/stylesheet.css">
+	
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+</head>
+<body>
+
+	<!-- Primary Page Layout
+	================================================== -->
+	
+	
+	<div id="intro">
+	
+		<div class="bg1"></div>
+		
+		<div class="title">
+        
+			<div class="intro-line"></div>
+            <img src="images/BarCamp_Logo_2013-White-V.png" alt="" class="blogo" style="margin-top:10px;margin-bottom: 10px;" />
+            <!--h1 class="smallplus">+</h1>
+			<h1 class="small">Code Camp</h1 -->
+            <!--h1 class="smallplusplus">October 13, 2012</h1 -->
+           
+            
+            
+			<!--div class="pushdown"></div -->
+			<div class="intro-line"></div>
+            
+            
+			<p class="tagline">A day by geeks, for geeks and about geeks on September 28th, 2013.</p>
+            
+            <div class="social-intro">
+			<ul>
+					<li><a href="http://twitter.com/BarCampTampa"><img src="images/icn-twitter-intro.png" alt="" /></a></li>
+					<li><a href="https://www.facebook.com/BarCampTampa"><img src="images/icn-facebook-intro.png" alt="" /></a></li>
+				
+					<li><a href="http://www.linkedin.com/groups/BarCamp-Tampa-Bay-983107"><img src="images/icn-linkedin-intro.png" alt="" /></a></li>
+					
+					<li><a href="http://vimeo.com/groups/barcamptampa"><img src="images/icn-vimeo-intro.png" alt="" /></a></li>
+                    
+                    <li><a href="http://www.flickr.com/groups/1468726@N24/"><img src="images/icn-flickr-intro.png" alt="" /></a></li>
+					
+				</ul>
+			</div>
+            
+			
+            <!--div class="social-intro">
+				<ul>
+					<li><a href="#"><img src="images/icn-twitter-intro.png" alt="" /></a></li>
+					<li><a href="#"><img src="images/icn-facebook-intro.png" alt="" /></a></li>
+					<li><a href="#"><img src="images/icn-gplus-intro.png" alt="" /></a></li>
+					<li><a href="#"><img src="images/icn-linkedin-intro.png" alt="" /></a></li>
+					<li><a href="#"><img src="images/icn-dribbble-intro.png" alt="" /></a></li>
+					<li><a href="#"><img src="images/icn-vimeo-intro.png" alt="" /></a></li>
+					<li><a href="#"><img src="images/icn-pinterest-intro.png" alt="" /></a></li>
+				</ul>
+			</div -->
+		</div> <!-- end title -->
+	</div> <!-- end intro -->
+	
+ 
+
+	
+<nav>
+     
+
+<div id="abovenav">
+			
+        <p>
+            <a href="https://barcamptampa.launchtrack.com/register/camp-2013" class="press-it-btn" target="_blank">Register Today!</a><br />
+            <!-- <a href="http://bit.ly/BCCC12_Schedule" style="color: white; font-size: 42px; text-decoration: none; margin-left: -100px; margin-top: 30px; position: absolute;" target="_blank">View Schedule</a> -->
+        </p>
+
+</div>
+                   
+<!--a href="http://barcampcodecamp2012.eventbrite.com/" target="_blank"><div class="button6">REGISTER NOW</div></a -->
+</div>     
+<!-- end added -->    
+   
+<div class="navbg">  
+
+ <ul>
+			<li><a href="#intro">Top</a></li>
+			<li><a href="#about">About the Event</a></li>
+	
+			<li><a href="#services">Sponsors</a></li>
+			<li><a href="#contact">Contact</a></li>
+		</ul>
+
+</div>  
+
+</nav>
+
+	
+<div id="about">
+
+		<div class="container">
+		
+			<div class="sixteen columns" span style="margin-bottom:20px;">
+
+				<p style="font-size:24px;color:#0a97f9;">Join us once again this year where the University of South Florida's College of Business will welcome us to their campus for a day of learning, sharing, networking and geeky fun.</p>
+            
+				<h2><span class="lines">About the Event</span></h2>
+
+<p><span class="blueprint">The event will be a BarCamp 7:30 am doors open and signups begin.</span><br /><br />
+Once again we will have a BarCamp Tampa event. It will be Tampa Bay’s largest tech event and will be held on the campus at the University of South Florida College of Business. There will be more announcements in the coming weeks and we expect an even larger attendance than last year so, register early and sign up for our mailing list.
+
+	<div class="title">
+		<iframe width="560" height="315" src="//www.youtube.com/embed/lado2tQkMK0" frameborder="0" allowfullscreen></iframe>
+	</div>
+</p>
+</div> <!-- end sixteen columns -->
+
+
+
+<!-- start 3 columns content -->
+<div class="one-third column">
+<!-- begin example gallery -->
+<div class="contentctr">
+<img src="images/learn.gif" class="scale-with-grid" alt="" />
+<p>The topics and the presenters are diverse, but will appeal to anyone who has an interest in technology and the internet. This is an event for web designers, developers, marketers, copy writers, SEOs, sys admins and anyone who operates a business with a web presence to get together and share their knowledge. The endearing quality of the unconference format is that you have options all day. If the presentation you choose isn’t what you were expecting, you can always jump over into another room and check out something else.</p>
+
+</div><!-- end contentctr -->
+
+</div><!-- end one-third column -->
+
+
+
+<!-- start 3 columns content -->
+<div class="one-third column">
+
+<div class="contentctr">
+<!-- begin example gallery -->
+<img src="images/share.gif" class="scale-with-grid" alt="" />
+
+<p>The schedule starts BLANK in the morning and is filled up by attendees that want to do a presentation. Presenters choose their room and time and everyone else scours the board to choose the path that they will take for the day. The day is what you make it, by choosing presentations to attend and deciding whether or not to present a topic. This is the best part of BarCamp. While most presenters prepare topics beforehand, there are sometimes impromptu topics from passionate people inspired by the feel of the day.</p>
+
+</div>
+
+</div><!-- end one-third column -->
+
+<!-- start 3 columns content -->
+<div class="one-third column">
+
+<div class="contentctr">
+
+<!-- begin example gallery -->
+<img src="images/support.gif" class="scale-with-grid" alt="" />
+
+<p>Barcamp is a community event, both in participation and in funding. It is made possible by the time and money donated by the crew of people running the event, the attendees and companies that support us. Thank you to all of you! Each year we have some fantastic companies that sponsor by donating time, money, facilities and supplies for the day. We’re also very lucky to get great support from attendees making donations in order to attend. We hope that everyone takes the time to check out the companies and people that support us!</p>
+
+</div>
+
+</div><!-- end one-third column -->
+
+
+<!-- end 3 columns -->
+
+<p>
+	Would you like to volunteer for part of the day? It’s a great way to get to know more of the people in the tech community. If so, <a href="https://barcamptampa.launchtrack.com/register/volunteers">register here</a>.
+</p>
+
+<div class="clear"></div>
+
+<br><br>
+    
+    </div> <!-- end container -->
+	</div> <!-- end about -->
+	
+	
+	<div id="separator1">
+		<div class="bg2"></div>
+       
+		<p class="separator">Support the companies that support<br />BarCamp Tampa 2013</p>
+	</div>
+	
+	
+	<div id="services">
+		<div class="container">
+		
+			<div class="sixteen columns">
+				<h2 class="white"><span class="lines">Our Premiere Sponsors</span></h2>
+			</div> <!-- end sixteen columns -->
+			
+			<div class="clear"></div>
+			
+	
+			
+            
+<div class="portfolio">
+			
+
+    
+    <div class="one-third column">
+					<!-- begin example gallery -->
+					<a href="http://www.usf.edu/" target="_blank"><img src="images/2013_logos/usf_business" class="scale-with-grid" alt="USF College of Business" /></a>
+				
+				</div> <!-- end one-third column -->
+			
+			<div class="one-third column">
+					<!-- begin example gallery -->
+					<a href="http://www.usf.edu/" target="_blank"><img src="images/2013_logos/usf.png" class="scale-with-grid" alt="USF" /></a>
+				
+				</div> <!-- end one-third column -->
+			
+			<div class="one-third column">
+					<!-- begin example gallery -->
+					&nbsp;
+				
+				</div> <!-- end one-third column -->
+
+				
+<br class="clear" />
+<br class="clear" />            
+				
+	</div> <!-- end portfolio -->
+
+
+<br class="clear" />
+<br class="clear" />	
+
+
+<!-- END NEW COL ++++++++
+++++++++++++++++++++++++++++++++++++-->
+
+    
+    
+				
+	</div> <!-- end portfolio -->
+            
+
+
+			
+			
+			
+<div class="clear"></div>
+			
+<br class="clear" />
+<br class="clear" />
+<br class="clear" />
+<br class="clear" />
+			
+		</div> <!-- end container -->
+        
+        
+	</div> <!-- end services -->
+	
+	
+	<div id="separator2">
+		<div class="bg3"></div>
+		
+	</div>
+	
+	
+	<div id="about">
+		<div class="container">
+		
+			<div class="sixteen columns">
+				<h2><span class="lines">Our Past Events</span></h2>
+			</div> <!-- end sixteen columns -->
+			
+			
+            
+       <div class="portfolio">
+			
+				<div class="one-third column">
+					<!-- begin example gallery -->
+					<a href="http://vimeo.com/12739522" target ="_blank"><img src="images/thumb1.jpg" class="scale-with-grid" alt="" /></a>
+				
+					<!-- end gallery -->
+					<p class="proj-title">2009 Barcamp Event</p>
+					<p class="proj-desc">Check out our video</p>
+				</div> <!-- end one-third column -->
+			
+				<div class="one-third column">
+						<!-- begin example gallery -->
+					<a href="http://www.flickr.com/photos/riverspring/sets/72157622334152987" title="2009 Barcamp" target="_blank"><img src="images/thumb2.jpg" class="scale-with-grid" alt="" /></a>
+		
+					<!-- end gallery -->
+					<p class="proj-title">2009 MOre Barcamp Event</p>
+					<p class="proj-desc">Other photos</p>
+				</div> <!-- end one-third column -->
+			
+				<div class="one-third column">
+					<a href="http://www.flickr.com/photos/markkrupinski/sets/72157607977674613" title="2008 Barcamp" target="_blank"><img src="images/thumb3.jpg" class="scale-with-grid" alt="" /></a>
+					<p class="proj-title">2008 Barcamp Event</p>
+					<p class="proj-desc">Check out our photos from this event</p>
+				</div> <!-- end one-third column -->
+				
+				<div class="clear"></div>				
+			
+			</div> <!-- end portfolio -->     
+            
+            
+            
+<div class="clear"></div>
+<br /><br />
+			
+			
+		
+			
+			
+			
+		</div> <!-- end container -->
+	</div> <!-- end about -->
+	
+	
+	<div id="separator3">
+		<div class="bg4"></div>
+		
+	</div>
+	
+	
+	<div id="contact">
+		<div class="container">
+		
+			<div class="sixteen columns">
+				<h2 class="white"><span class="lines">Contact Us</span></h2>
+			</div> <!-- end sixteen columns -->
+			
+			<div class="six columns">
+  
+
+<p class="white">Send us an email</p>
+                
+        
+
+                
+ <!-- contact -->
+ 	 <div class="contact-form">
+					<a href="mailto:bg.young2@gmail.com"><div class="btn btn-info btn-lg">Contact Us!</div></a>
+				</div>
+					<!-- <div class="done">
+						<b>Thank you!</b> I have received your message. 
+					</div>
+				
+					<form method="post" action="process.php">
+						<p>name</p>
+						<input type="text" name="name" class="text" />
+						
+						<p>email</p>
+						<input type="text" name="email" class="text" id="email" />
+
+						<p>message</p>
+						<textarea name="comment" class="text"></textarea>
+
+						<input type="submit" id="submit" value="send" class="submit-button" />
+					</form>
+						
+				</div>  --> <!-- end contact-form -->
+ <!-- end contact -->               
+                <div class="clear"></div>
+            </div>
+
+              
+                
+                
+			 
+			
+			<div class="eight columns">
+				
+                <div class="callout">
+            
+            <div class ="entrytitle">EVENT LOCATION</div>
+            
+<!-- googole API -->
+<iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;q=USF+College+of+Business&amp;fb=1&amp;cid=0,0,6602113154269805579&amp;near=Tampa,+FL&amp;t=m&amp;ll=28.061491,-82.408812&amp;spn=0.006627,0.00912&amp;z=16&amp;iwloc=A&amp;output=embed"></iframe>
+<!-- end API -->
+           
+           
+       <br>
+<br>
+    
+           
+          <a href="https://maps.google.com/maps?hl=en&ie=UTF8&q=USF+College+of+Business&fb=1&cid=0,0,6602113154269805579&near=Tampa,+FL&t=m&ll=28.061491,-82.408812&spn=0.006627,0.00912&z=16&iwloc=A&source=embed" target="_blank"> <div class="button4">VIEW LARGER MAP</div></a>
+            </div>
+            
+            
+            </div>
+            <!-- end add -->    
+                
+                
+			</div>
+			
+			<div class="clear"></div>
+			
+			
+
+			
+			<div class="sixteen columns">
+				<div class="copyright">
+					<p>BarCamp Tampa Bay is September 28th at the University of South Florida (USF) in Tampa, FL<br>
+&copy; 2013 All Rights Reserved<br>
+</p>
+				</div>
+			</div>
+			
+			
+		</div> <!-- end container -->
+		
+	</div> <!-- end contact -->
+	
+
+	
+
+
+	<!-- JS
+	================================================== -->
+	<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+	<script src="javascripts/jquery.sticky.js" type="text/javascript"></script>
+	<script src="javascripts/nbw-parallax.js" type="text/javascript"></script>
+	<script src="javascripts/jquery.inview.js" type="text/javascript"></script>
+	<script src="javascripts/smooth-scroll.js" type="text/javascript"></script>
+	<script src="javascripts/jquery.flexslider.js" type="text/javascript"></script>
+	<script src="javascripts/jquery.prettyPhoto.js" type="text/javascript"></script>
+	<script src="contact-form.js"></script>
+	
+	<!-- Sticky nav -->
+	<script>
+		$(document).ready(function(){
+			$("nav").sticky({topSpacing:0});
+		});
+	</script>
+	
+	<!-- smooth scroll 
+	<script>
+	    $(document).ready(function() {
+
+		    $('nav a').smoothScroll();
+        });
+    </script>-->
+	
+	<!-- slider -->
+	<script>
+		$(window).load(function() {
+			$('.flexslider').flexslider({
+				animation: "slide",
+				slideshow: true,
+				slideshowSpeed: 3500,
+				animationSpeed: 1000
+			});
+		});
+	</script>
+	
+	<!-- pretty photo 
+	<script>
+		$(document).ready(function(){
+			$("a[class^='prettyPhoto']").prettyPhoto({
+				social_tools: false,
+				theme: 'light_square'
+			});
+		});
+	</script>
+	-->
+	
+	<script type="text/javascript" charset="utf-8">
+		$(document).ready(function(){
+			$("a[rel^='prettyPhoto']").prettyPhoto({
+				social_tools: false,
+				theme: 'light_square'
+			});
+		});
+	</script>
+
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
